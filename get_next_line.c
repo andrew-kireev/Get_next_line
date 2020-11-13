37f37cd26d1bd8731157510a9d832cd45afff2fc
Andrew <a-kireev1989@mail.ru>
2020-11-14 00:57:44 +0300

    Complete norrminating project

--- get_next_line.c
+++ get_next_line.c
@@ -11,13 +11,12 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void clear_buffer(char *str, size_t size)
+void		clear_buffer(char *str, size_t size)
 {
-	size_t 		i;
+	size_t		i;
 
 	i = 0;
 	while (i != size)
@@ -27,89 +26,80 @@ void clear_buffer(char *str, size_t size)
 	}
 }
 
-int  read_file(int fd, char **lines_fd)
+int			read_file(int fd, char **lines_fd)
 {
-    char        buf[BUFFER_SIZE + 1];
-    int         bytes_read;
-    char		*tmp;
-    int			result;
+	char		buf[BUFFER_SIZE + 1];
+	int			bytes_read;
+	char		*tmp;
+	int			result;
 
-    result = 0;
+	result = 0;
 	if (lines_fd[fd] == NULL)
 		lines_fd[fd] = ft_strdup("");
 	clear_buffer(buf, BUFFER_SIZE + 1);
-    while ((bytes_read = read(fd, buf, BUFFER_SIZE)) > 0)
+	while ((bytes_read = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
-    	result += bytes_read;
-    	buf[bytes_read] = '\0';
-    	tmp = ft_strjoin(lines_fd[fd], buf);
-    	free(lines_fd[fd]);
-    	lines_fd[fd] = tmp;
-    	if (ft_strrchr(lines_fd[fd], '\n') != NULL) {
-    		return result;
-    	}
-    	clear_buffer(buf, BUFFER_SIZE + 1);
-    }
-    if (bytes_read == -1)
-    	result = -1;
-    return (result);
+		result += bytes_read;
+		buf[bytes_read] = '\0';
+		tmp = ft_strjoin(lines_fd[fd], buf);
+		free(lines_fd[fd]);
+		lines_fd[fd] = tmp;
+		if (ft_strrchr(lines_fd[fd], '\n') != NULL)
+			return (result);
+		clear_buffer(buf, BUFFER_SIZE + 1);
+	}
+	if (bytes_read == -1)
+		result = -1;
+	return (result);
 }
 
-int check_line(char *line) {
+int			check_line(char *line)
+{
 	char	*ptr;
 
 	if (line == NULL)
 		return (-1);
-
-	if ((ptr = ft_strrchr(line, '\n')) != NULL) {
+	if ((ptr = ft_strrchr(line, '\n')) != NULL)
+	{
 		*ptr = '\0';
-			return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
 
-char	*clear_lines(char **lines_fd, int start, int fd) {
+char		*clear_lines(char **lines_fd, int start, int fd)
+{
 	char *tmp;
+
 	if (lines_fd[fd][start] == '\n' && ft_strlen(lines_fd[fd]) > start)
 		start++;
 	tmp = ft_strdup(&(lines_fd[fd][start]));
 	free(lines_fd[fd]);
 	lines_fd[fd] = tmp;
-	return tmp;
+	return (tmp);
 }
 
-int find_end_(const char *str)
+int			get_next_line(int fd, char **line)
 {
-	int		i;
-	if (str == NULL || ft_strlen(str) == 0)
-		return 0;
+	static char	*lines_fd[FD_NUMS];
+	int			reading_result;
+	int			return_value;
+	int			end;
 
-	i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
-		i++;
-//	i++;
-	return (i);
-}
-
-int         get_next_line(int fd, char **line)
-{
-	static char *lines_fd[FD_NUMS];
-    int         reading_result;
-    int			return_value;
-
-    return_value = 0;
-    if ((fd < 0 || !line || BUFFER_SIZE == 0) != 0)
-        return (-1);
-    reading_result = read_file(fd, lines_fd);
-
-    if (reading_result >= 0) {
-    	*line = ft_substr(lines_fd[fd], 0, find_end_(lines_fd[fd]) + 1);
-    	return_value = check_line(*line);
-//		printf("%s\n", lines_fd[fd]);
-    	clear_lines(lines_fd, ft_strlen(*line), fd);
-//    	printf("%s\n", *line);
-    }
-    if (reading_result == -1)
-    	return_value = -1;
-    return return_value;
+	return_value = 0;
+	end = 0;
+	if ((fd < 0 || !line || BUFFER_SIZE == 0) != 0)
+		return (-1);
+	reading_result = read_file(fd, lines_fd);
+	if (reading_result >= 0)
+	{
+		while (lines_fd[fd][end] != '\0' && lines_fd[fd][end] != '\n')
+			end++;
+		*line = ft_substr(lines_fd[fd], 0, end + 1);
+		return_value = check_line(*line);
+		clear_lines(lines_fd, ft_strlen(*line), fd);
+	}
+	if (reading_result == -1)
+		return_value = -1;
+	return (return_value);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -24,7 +24,10 @@ char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strdup(const char *str);
 int			ft_strlen(const char *str);
 int			read_file(int fd, char **lines_fd);
-char 		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strrchr(char *str, char ch);
+char		*clear_lines(char **lines_fd, int start, int fd);
+int			check_line(char *line);
+void		clear_buffer(char *str, size_t size);
 
 #endif
